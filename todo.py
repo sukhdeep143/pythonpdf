f5bdddbedf0e8d3f3617b63f49f4043184c8f84e
Sukhdeep singh <Sukhdeepsingh141414@gmail.com>
2025-02-02 20:44:17 +0530

    Add task and view task

--- todo.py
+++ todo.py
@@ -4,19 +4,30 @@ def add_task(task):
     tasks.append(task)
     print(f"Task '{task}' added ")
     
-def view_task():
+def view_tasks():
     if not tasks:
         print("No task founded")
     else:
         for i, task in enumerate(tasks, start=1):
             print(f"{i}. {task}")
-
-task1 = input("Enter a task to add in the list")
-add_task(task1)
+            
 
 
-show =view_task()
+# Main program loop
+while True:
+    print("\nTo-Do List Menu:")
+    print("1. Add Task")
+    print("2. View Tasks")
 
-print(show)
+    
+    choice = input("Enter your choice (1 or 2): ")
+    
+    if choice == "1":
+        task = input("Enter the task: ")
+        add_task(task)
+    elif choice == "2":
+        view_tasks()
+    
+    
 
 
